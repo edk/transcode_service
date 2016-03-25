cb162dc5abd93fe17ba55e3fa195fd38d2b0bc32
Eddy Kim <eddyhkim@gmail.com>
2016-03-25 16:03:24 -0700

    forgot the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :transcode_jobs, except: [:new, :edit]
+    resources :transcode_jobs, except: [:new, :edit] do
+      get :ping, :on => :collection
+    end
   end
 end
